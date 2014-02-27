3271c01389a4050692c4e9b032eb820a64075dbc
Emily Martinez <mle626@gmail.com>
2014-02-27 04:32:03 -0800

    Fakebook Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fakebook::Application.config.secret_key_base = '1116779d8e0904d6617a4b4b4f8460b00b66f79651fd0d97f86c4db03cde6394645509aaecefde21c4b8b64fcdc1582499cab2a25ebcf5c518156e90c24892f4'
