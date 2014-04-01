6a05ca2289d3fc11a1ebe7ccf22e027e4a66fcb8
Emily Martinez <mle626@gmail.com>
2014-04-01 12:32:59 -0700

    Add user friendships and associated tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  should have_many(:user_friendships)
+  should have_many(:friends)
+
   test "a user should enter a first name" do
   	user = User.new
   	assert !user.save
@@ -35,6 +39,18 @@ class UserTest < ActiveSupport::TestCase
 		assert !user.errors[:profile_name].empty?
 		assert user.errors[:profile_name].include?("Must be formatted correctly.") 
 	end
+
+  test "that no error is raised when trying to access a friend list" do
+    assert_nothing_raised do
+      users(:emily).friends
+    end
+  end
+
+  test "that creating friendships on a user works" do
+    users(:emily).friends << users(:bowie)
+    users(:emily).friends.reload
+    assert users(:emily).friends.include?(users(:bowie))
+  end
 
  #  test "a user can have a correctly formatted profile name" do
  #    user = User.new(first_name: 'Emily', last_name: 'Martinez', email: 'mle626@gmail.com')
